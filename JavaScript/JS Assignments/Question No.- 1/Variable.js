730e0df23ca0121a5203b38e5398498b4537bd58
Ran-Veer <rk709042@gmail.com>
2023-05-04 23:02:02 +0530

    javascript

---
+++ JavaScript/JS Assignments/Question No.- 1/Variable.js
@@ -0,0 +1,21 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// string Type
+
+let TeamName = "CSK"
+console.log(`The variable ${TeamName} is ${typeof(TeamName)} Type.`)
+
+// Boolean Type
+
+let CSK_wins = true
+console.log(`The variable ${CSK_wins} is ${typeof(CSK_wins)} Type.`)
+
+// Undefined Type
+
+let Wining_prize
+console.log(`The variable Wining_prize is ${typeof(Wining_prize)} Type.`)
+
+// Null Data Types
+
+let Match_Draw_Result = null
+console.log(`The variable ${Match_Draw_Result} is ${typeof(Match_Draw_Result)} Type.`)
---
+++ JavaScript/JS Practice/practice.js
@@ -0,0 +1,21 @@
+// Practice Session
+
+// string Type
+
+let TeamName = "CSK"
+console.log(`The variable ${TeamName} is ${typeof(TeamName)} Type.`)
+
+// Boolean Type
+
+let CSK_wins = true
+console.log(`The variable ${CSK_wins} is ${typeof(CSK_wins)} Type.`)
+
+// Undefined Type
+
+let Wining_prize
+console.log(`The variable Wining_prize is ${typeof(Wining_prize)} Type.`)
+
+// Null Data Types
+
+let Match_Draw_Result = null
+console.log(`The variable ${Match_Draw_Result} is ${typeof(Match_Draw_Result)} Type.`)
