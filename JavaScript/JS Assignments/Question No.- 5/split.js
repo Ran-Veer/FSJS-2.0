e5a1a972389918c99577b60eb3d510da9ec7730a
Ran-Veer <rk709042@gmail.com>
2023-05-05 01:05:54 +0530

    Q-5

---
+++ JavaScript/JS Assignments/Question No.- 5/split.js
@@ -0,0 +1,5 @@
+// 05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+let data = "method takes a pattern and divides a String into an ordered list"
+const convert = data.split(" ") 
+console.log(convert)
