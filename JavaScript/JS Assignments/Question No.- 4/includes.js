273df64fdb0b356441647e8377010dd4431156f8
Ran-Veer <rk709042@gmail.com>
2023-05-05 00:48:52 +0530

    Q-4

---
+++ JavaScript/JS Assignments/Question No.- 4/includes.js
@@ -0,0 +1,4 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+let JavaScript = "scripttype. Specifies the type of the script"
+console.log(JavaScript.includes("script"))
