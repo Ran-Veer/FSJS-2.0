c0c0a3132344c18a5d93bbd9557bb2f429155371
Ran-Veer <rk709042@gmail.com>
2023-05-05 00:11:54 +0530

    q-2

--- JavaScript/JS Assignments/Question No.- 2/interploation method.js
+++ JavaScript/JS Assignments/Question No.- 2/interploation method.js
@@ -1,1 +1,9 @@
 // 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+const First_Name = "Ranveer"
+const Last_Name = "Kumar"
+let Marital_Status = "Unmarried"
+const Country = "INDIA"
+let AGE = 26
+
+console.log(`Hello, My Name Is ${First_Name} ${Last_Name}, My Country Is ${Country} And I Am ${AGE}yr Old ${Marital_Status}.`)
--- JavaScript/JS Practice/practice.js
+++ JavaScript/JS Practice/practice.js
@@ -2,20 +2,28 @@
 
 // string Type
 
-let TeamName = "CSK"
-console.log(`The variable ${TeamName} is ${typeof(TeamName)} Type.`)
+// let TeamName = "CSK"
+// console.log(`The variable ${TeamName} is ${typeof(TeamName)} Type.`)
 
-// Boolean Type
+// // Boolean Type
 
-let CSK_wins = true
-console.log(`The variable ${CSK_wins} is ${typeof(CSK_wins)} Type.`)
+// let CSK_wins = true
+// console.log(`The variable ${CSK_wins} is ${typeof(CSK_wins)} Type.`)
 
-// Undefined Type
+// // Undefined Type
 
-let Wining_prize
-console.log(`The variable Wining_prize is ${typeof(Wining_prize)} Type.`)
+// let Wining_prize
+// console.log(`The variable Wining_prize is ${typeof(Wining_prize)} Type.`)
 
-// Null Data Types
+// // Null Data Types
 
-let Match_Draw_Result = null
-console.log(`The variable ${Match_Draw_Result} is ${typeof(Match_Draw_Result)} Type.`)
+// let Match_Draw_Result = null
+// console.log(`The variable ${Match_Draw_Result} is ${typeof(Match_Draw_Result)} Type.`)
+
+const First_Name = "Ranveer"
+const Last_Name = "Kumar"
+let Marital_Status = "Unmarried"
+const Country = "INDIA"
+let AGE = 26
+
+console.log(`Hello, My Name Is ${First_Name} ${Last_Name}, My Country Is ${Country} And I Am ${AGE}yr Old ${Marital_Status}.`)
