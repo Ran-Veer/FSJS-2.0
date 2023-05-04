bd3ad81255ca1ecc689d10b84e674b687270aed7
Ran-Veer <rk709042@gmail.com>
2023-05-05 00:28:00 +0530

    touppercase

--- JavaScript/JS Practice/practice.js
+++ JavaScript/JS Practice/practice.js
@@ -20,10 +20,13 @@
 // let Match_Draw_Result = null
 // console.log(`The variable ${Match_Draw_Result} is ${typeof(Match_Draw_Result)} Type.`)
 
-const First_Name = "Ranveer"
-const Last_Name = "Kumar"
-let Marital_Status = "Unmarried"
-const Country = "INDIA"
-let AGE = 26
+// const First_Name = "Ranveer"
+// const Last_Name = "Kumar"
+// let Marital_Status = "Unmarried"
+// const Country = "INDIA"
+// let AGE = 26
 
-console.log(`Hello, My Name Is ${First_Name} ${Last_Name}, My Country Is ${Country} And I Am ${AGE}yr Old ${Marital_Status}.`)
+// console.log(`Hello, My Name Is ${First_Name} ${Last_Name}, My Country Is ${Country} And I Am ${AGE}yr Old ${Marital_Status}.`)
+
+let To_UpperCase = "The toUpperCase() method converts a string to uppercase letters."
+console.log(To_UpperCase.toUpperCase())
