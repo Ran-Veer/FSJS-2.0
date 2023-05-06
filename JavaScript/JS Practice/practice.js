b59ff1569134417f81c6e9adb55834be329b6f22
Ran-Veer <rk709042@gmail.com>
2023-05-07 00:25:45 +0530

    indexof

--- JavaScript/JS Practice/practice.js
+++ JavaScript/JS Practice/practice.js
@@ -43,3 +43,6 @@
 // console.log(array)
 
 
+// let IPLTeams = ['pw skills', 'CSK','DC', 'MI', 'PBKS', 'pw skills', 'RR', 'RCB', 'pw skills', 'LSG', 'GT', 'SRH', 'KKR', 'pw skills'];
+// console.log(IPLTeams.indexOf('pw skills'))
+// console.log(IPLTeams.lastIndexOf('pw skills'))
