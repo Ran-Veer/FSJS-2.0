99855c69780934ad32a064d84c2f71c86a47fd88
Ran-Veer <rk709042@gmail.com>
2023-05-07 00:36:28 +0530

    lastindexof

---
+++ JavaScript/JS Assignments/Question No.- 7/lastindexof.js
@@ -0,0 +1,6 @@
+//07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+
+let IPLTeams = ['pw skills', 'CSK','DC', 'MI', 'PBKS', 'pw skills', 'RR', 'RCB', 'pw skills', 'LSG', 'GT', 'SRH', 'KKR', 'pw skills'];
+console.log(IPLTeams.indexOf('pw skills'))
+console.log(IPLTeams.lastIndexOf('pw skills'))
